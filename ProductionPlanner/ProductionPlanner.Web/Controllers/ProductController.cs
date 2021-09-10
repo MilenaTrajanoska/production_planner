e58fe0e14dbe4ad725e3d9c63e6dcc51ca472d11
Anastasija99 <anastasija.petrovska.5@gmail.com>
2021-09-10 23:46:43 +0200

    Created ProductController and view for new product
    Discuss view elements and design

---
+++ ProductionPlanner/ProductionPlanner.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductionPlanner.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //to be discussed
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
