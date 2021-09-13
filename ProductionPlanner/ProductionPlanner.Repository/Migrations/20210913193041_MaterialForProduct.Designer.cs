536c8fbe59893aed69e7e923c649a305e16cfb70
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2021-09-13 21:37:38 +0200

    Added relation MaterialForProduct

--- ProductionPlanner/ProductionPlanner.Repository/Migrations/20210913193041_MaterialForProduct.Designer.cs
+++ ProductionPlanner/ProductionPlanner.Repository/Migrations/20210913193041_MaterialForProduct.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductionPlanner.Repository.Data;
 
 namespace ProductionPlanner.Repository.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210913193041_MaterialForProduct")]
+    partial class MaterialForProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
