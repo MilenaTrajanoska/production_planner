5544b73c1bd523b0a98a37d4b71934689aff3891
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2021-09-19 14:24:36 +0200

    created import orders from spreadsheet feature

---
+++ ProductionPlanner/ProductionPlanner.Domain/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionPlanner.Domain.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
