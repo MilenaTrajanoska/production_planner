b68e62303c07f2792dc62582aae8ad67546613b3
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2021-10-07 18:37:33 +0200

    Create CacheKeys.cs

---
+++ ProductionPlanner/ProductionPlanner.Domain/CacheKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductionPlanner.Domain
+{
+    public static class CacheKeys
+    {
+        public static String GlobalPerformance = "Global_Performance";
+        public static String GraphData = "Graph_Data";
+    }
+}
