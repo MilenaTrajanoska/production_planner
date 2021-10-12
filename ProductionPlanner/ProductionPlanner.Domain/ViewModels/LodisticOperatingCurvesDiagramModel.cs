9610c25aff5d058eb0013750e899cbc4a98701e9
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2021-10-12 23:32:16 +0200

    Update models

--- ProductionPlanner/ProductionPlanner.Domain/ViewModels/LodisticOperatingCurvesDiagramModel.cs
+++ ProductionPlanner/ProductionPlanner.Domain/ViewModels/LodisticOperatingCurvesDiagramModel.cs
@@ -6,25 +6,31 @@ namespace ProductionPlanner.Domain.ViewModels
 {
     public class LodisticOperatingCurvesDiagramModel
     {
-        private List<double> locCapacity;
-
+        public List<double> Labels { get; set; }
         public List<double> OutputRate { get; set; }
         public List<double> ThroughputTime { get; set; }
-        public double Range { get; set; }
+        public double RangeX { get; set; }
+        public double RangeY { get; set; }
         public List<double> Capacity { get; set; }
         public List<double> OPOperatingPoin { get; set; }
-        public double OPRange { get; set; }
-        public double OPThroughputTime { get; set; }
+        public double OPRangeX { get; set; }
+        public double OPRangeY { get; set; }
+        public double OPThroughputTimeX { get; set; }
+        public double OPThroughputTimeY { get; set; }
 
-        public LodisticOperatingCurvesDiagramModel(List<double> outputRate, List<double> throughputTime, double range, List<double> locCapacity, List<double> opOperatingPoin, double opRange, double opThroughputTime)
+        public LodisticOperatingCurvesDiagramModel(List<double> labels, List<double> outputRate, List<double> throughputTime, double rangeX, double rangeY, List<double> locCapacity, List<double> opOperatingPoin, double opRangeX, double opRangeY, double opThroughputTimeX, double opThroughputTimeY)
         {
+            this.Labels = labels;
             this.OutputRate = outputRate;
             this.ThroughputTime = throughputTime;
-            this.Range = range;
+            this.RangeX = rangeX;
+            this.RangeY = rangeY;
             this.Capacity = locCapacity;
             this.OPOperatingPoin = opOperatingPoin;
-            this.OPRange = opRange;
-            this.OPThroughputTime = opThroughputTime;
+            this.OPRangeX = opRangeX;
+            this.OPRangeY = opRangeY;
+            this.OPThroughputTimeX = opThroughputTimeX;
+            this.OPThroughputTimeY = opThroughputTimeY;
         }
     }
 }
--- ProductionPlanner/ProductionPlanner.Domain/ViewModels/ScheduleReliabilityOperatingCurveDiagramModel.cs
+++ ProductionPlanner/ProductionPlanner.Domain/ViewModels/ScheduleReliabilityOperatingCurveDiagramModel.cs
@@ -6,11 +6,13 @@ namespace ProductionPlanner.Domain.ViewModels
 {
     public class ScheduleReliabilityOperatingCurveDiagramModel
     {
+        public List<double> Labels { get; set; }
         public List<double> ScheduleReliability { get; set; }
         public List<double> MeanWIP { get; set; }
 
-        public ScheduleReliabilityOperatingCurveDiagramModel(List<double> scheduleReliability, List<double> meanWIP)
+        public ScheduleReliabilityOperatingCurveDiagramModel(List<double> labels, List<double> scheduleReliability, List<double> meanWIP)
         {
+            this.Labels = labels;
             this.ScheduleReliability = scheduleReliability;
             this.MeanWIP = meanWIP;
         }
